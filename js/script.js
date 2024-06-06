b1e14854ea813be1b477aee215f0760f10618b39
Tengen777 <theo30m@gmail.com>
2024-06-06 21:02:30 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 // script.js
 window.onload = ()=> {
 console.log("script linked");
+
+    fetchData();
+
 };
+
+async function fetchData() {
+    fetch('data.json')
+        .then(response => response.json())
+        .then(data => displayStudentData(data))
+        .catch(error => console.error('Error fetching data:', error));
+}
+
+
 
