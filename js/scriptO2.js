5ba71df374a113ba6916393cfe2fb93d9c292672
Tengen777 <theo30m@gmail.com>
2024-06-07 02:12:46 +0200

    Update scriptO2.js
    eventlistnenrs + functies selectGroep en selectLeerlingen

--- js/scriptO2.js
+++ js/scriptO2.js
@@ -1,4 +1,29 @@
 window.onload = async () => {
 
+    let results = await fetch('data.json');
+    data = await results.json();
+
+
+
+    document.getElementById('klasgroep')
+    .addEventListener('change', (event) => {
+       vulLeerlingen(event.target.value);
+    });
+
+    document.getElementById('leerlingen')
+    .addEventListener('change', (event) => {
+    });
     
 };
+
+
+function selectgroep(){
+
+}
+
+
+
+let selectLeerlingen()=> {
+
+
+}
