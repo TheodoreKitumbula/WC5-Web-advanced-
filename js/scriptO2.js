4ad58a4d16e1661d1e39b16bd6ba40d4282e34d1
Tengen777 <theo30m@gmail.com>
2024-06-07 02:26:33 +0200

    Update scriptO2.js
    dropdown voor klasgroep +option tag

--- js/scriptO2.js
+++ js/scriptO2.js
@@ -8,6 +8,7 @@ window.onload = async () => {
     document.getElementById('klasgroep')
     .addEventListener('change', (event) => {
        vulLeerlingen(event.target.value);
+       
     });
 
     document.getElementById('leerlingen')
@@ -18,12 +19,18 @@ window.onload = async () => {
 
 
 function selectgroep(){
+    let klasgroep = document.getElementById('klasgroep');
+    for (const klas in data) {
+        const option = document.createElement('option');
+        option.value = klas;
+        option.textContent = klas;
+        klasgroepDropdown.appendChild(option);
 
 }
 
 
 
-let selectLeerlingen()=> {
+let selectLeerlingen ()=> {
 
 
 }
